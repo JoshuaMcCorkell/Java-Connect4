4c536d16b1fa30bfbada63735b46de2ff86cc01b
Joshua McCorkell <joshua@themccorkells.com>
2022-03-01 17:45:07 +1000

    Initial Commit

---
+++ C4Game.java
@@ -0,0 +1,11 @@
+public class C4Game {
+    private Column[] data;
+
+    public C4Game() {
+        data = new Column[] {
+            new Column(), new Column(), new Column(), 
+            new Column(), new Column(), 
+            new Column(), new Column(),
+        };
+    }
+}
