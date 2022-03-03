a975ff9f68aea81e108ee22da10108ce4b4f800b
Joshua McCorkell <joshua@themccorkells.com>
2022-03-03 19:14:26 +1000

    Adding horizontal win checking

--- Connect4/Connect4.java
+++ Connect4/Connect4.java
@@ -1,32 +1,64 @@
 package connect4;
 import java.util.HashMap;
-import java.lang.StringBuilder;
-public class Connect4 {
-    static final int HEIGHT = 6;
-    static final int COLUMNS = 7;
-    static final char[] SYMBOL = new char[] {'.', 'R', 'Y'};
 
-    private Position current;
-    private int turn;
-    
+public class Connect4 {
+    static final int ROWS = 6;
+    static final int COLUMNS = 7;
+    static final int TOWIN = 4;
+    static final String[] SYMBOL = new String[] {".", "R", "Y"};
+
+    public Position current;
+    public int turn;
+    public int winner;
+
     public Connect4() {
-        current = new Position(COLUMNS, HEIGHT);
+        current = new Position(COLUMNS, ROWS);
         turn = 1;
     }
 
     public void play(int column) {
         current.push(column, turn);
         turn = 3 - turn;
-        //TODO add a win check here
+        winner = checkWin();
+        if (winner != 0) {
+            System.out.print(SYMBOL[winner] + " Wins!!! Played:");System.out.println(column);
+        }
     }
 
+    public int checkWin() {
+        int n;
+        for (int i = 0; i < ROWS; i++) { // Loops through the rows
+            for (int j = 0; j < COLUMNS - TOWIN + 1; j++) { 
+            // Loops through the column that will be the START of the potential win position.
+                n = 1;
+                for (int k = 0; k < TOWIN; k++) { 
+                /* 
+                Loops through the columns in the potential win position,
+                multiplying n by the value in each position. If n is 0, 
+                no one has won. If not, it checks if one of the players has won. 
+                */
+                    n *= current.get(j + k, i);
+                }
+                if (n == 1) {
+                    return 1;
+                } else if (n > Math.pow(2.0, TOWIN) - 0.5) { //Check an inequality to overcome float innaccuracy.
+                    return 2;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
 
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder(100);
         //Build the position, row by row.
         str.append("Current Position:\n");
-        for (int i = HEIGHT - 1; i > -1; i--) {
+        for (int i = ROWS - 1; i > -1; i--) {
             for (int j = 0; j < COLUMNS; j++) {
                 str.append(SYMBOL[current.get(j, i)] + " ");
             }
--- Main.java
+++ Main.java
@@ -3,12 +3,18 @@ import connect4.Connect4;
 public class Main {
     public static void main(String[] args) {
         Connect4 game = new Connect4();
-        System.out.println(game);
+        game.print();
         game.play(1);
+        game.play(1);
+        game.play(2);
+        game.play(2);
         game.play(3);
-        game.play(0);
         game.play(3);
-        game.play(3);
-        System.out.println(game);
+        game.play(6);
+        game.play(4);
+        game.play(6);
+        game.print();
+        game.play(4);
+        game.print();
     }
 }
